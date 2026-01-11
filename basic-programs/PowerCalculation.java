73a94110d50faff0427305de425692df917fa522
codekuhu <singhayushi9452@gmail.com>
2026-01-11 23:22:38 +0530

    Added PowerCalculation program

---
+++ basic-programs/PowerCalculation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PowerCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter base:");
+        double base = sc.nextDouble();
+
+        System.out.println("Enter exponent:");
+        double exp = sc.nextDouble();
+
+        double result = Math.pow(base, exp);
+        System.out.println(result);
+    }
+}
